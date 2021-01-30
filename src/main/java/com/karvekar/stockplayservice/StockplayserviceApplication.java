a15ff9eabe16476668c6f78549355093de4c7500
sanketkarvekar <sksanket7@gmail.com>
2021-01-30 10:37:33 +0530

    first commit

---
+++ src/main/java/com/karvekar/stockplayservice/StockplayserviceApplication.java
@@ -0,0 +1,13 @@
+package com.karvekar.stockplayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockplayserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockplayserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karvekar/stockplayservice/StockplayserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karvekar.stockplayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockplayserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
